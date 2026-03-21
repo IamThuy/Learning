1322cc5fbee83a8ba8b66f57fef9e935c90c078a
Thuy <glithbr9@gmail.com>
2026-03-21 20:41:58 -0300

    Add files via upload

---
+++ CsharpLearning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new HttpClient();
+        string caminho = "info.txt";
+
+        string usuario = Environment.UserName;
+        string maquina = Environment.MachineName;
+        string os = Environment.OSVersion.ToString();
+        string cpu = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER")!;
+        string ip =  await client.GetStringAsync("https://api.ipify.org");
+
+        string conteudo = $@"
+                \\\ info ///
+            {usuario}
+            {maquina}
+            {os}
+            {cpu}
+            {ip}
+        ";
+
+        File.WriteAllText(caminho, conteudo);
+
+    }
+}
